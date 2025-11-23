b8753a6836ed3362a8b23fafa3b6c6c530d6ca08
am-mahmud <am.mahmudasif@gmail.com>
2025-11-23 05:08:45 -0500

    About page added

--- src/app/(public)/about/page.jsx
+++ src/app/(public)/about/page.jsx
@@ -1,9 +1,10 @@
+import ContentSection from '@/components/content-5';
 import React from 'react';
 
 const page = () => {
     return (
         <div className='relative mx-auto max-w-6xl min-h-screen px-12 pt-24'>
-            <h1>This is about page</h1>
+            <ContentSection></ContentSection>
         </div>
     );
 };
