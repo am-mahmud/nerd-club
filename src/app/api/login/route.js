7ea3522ba2c8bed06944b18a0c57c4c37e843e67
am-mahmud <am.mahmudasif@gmail.com>
2025-11-24 22:16:50 -0500

    login route set

---
+++ src/app/api/login/route.js
@@ -0,0 +1,36 @@
+import bcrypt from "bcryptjs";
+import connectDB from "@/lib/connectDB";
+import { NextResponse } from "next/server";
+import User from "@/models/User";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+    const { email, password } = await req.json();
+
+    // Find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json({ error: "User not found" }, { status: 400 });
+    }
+
+    // Compare password
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return NextResponse.json({ error: "Wrong password" }, { status: 400 });
+    }
+
+    return NextResponse.json({
+      success: true,
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
