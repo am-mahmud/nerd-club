984bc3afbaa13b1db8eb55aebd31ba62171a53d8
am-mahmud <am.mahmudasif@gmail.com>
2025-11-25 10:04:03 -0500

    File updated

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    votes: {
-        type: Number,
-        default: 0,
-    }
-});
+  title: String,
+  description: String,
+  votes: { type: Number, default: 0 },
+
+  // Map of userId (string) -> vote value (1 or -1)
+  userVotes: {
+    type: Map,
+    of: Number,
+    default: {}
+  }
+}, { timestamps: true });
 
 export default mongoose.models.Post || mongoose.model("Post", PostSchema);
