14813cfd35c42b558479c19e39260da1d8551f7a
am-mahmud <am.mahmudasif@gmail.com>
2025-11-25 00:49:45 -0500

    Edit post server & client updated

--- src/app/(protected)/edit-post/[id]/page.jsx
+++ src/app/(protected)/edit-post/[id]/page.jsx
@@ -1,30 +1,12 @@
-"use client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import EditPostClient from "./EditPostClient";
 
-import { useParams } from "next/navigation";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { Button } from "@/components/ui/button";
+export default async function EditPostPage({ params }) {
+  const session = await getServerSession(authOptions);
 
+  if (!session) redirect("/login");
 
-export default function EditPost() {
-  // const { id } = useParams();
-
-
-  return (
-    <div className="mx-auto max-w-6xl min-h-screen px-12 pt-24">
-      <h1 className="text-2xl font-semibold mb-4">Edit Post</h1>
-
-      <div className="space-y-4">
-
-        <Input placeholder="Post title..." />
-
-        <Textarea
-          className="h-40"
-          placeholder="Post content..."
-        />
-
-        <Button>Update</Button>
-      </div>
-    </div>
-  );
+  return <EditPostClient />;
 }
