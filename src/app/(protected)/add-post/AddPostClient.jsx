8455348c01f5517cdea6e37b5d4c5ef08b5c33c9
am-mahmud <am.mahmudasif@gmail.com>
2025-11-25 00:52:47 -0500

    Add post server and client added

---
+++ src/app/(protected)/add-post/AddPostClient.jsx
@@ -0,0 +1,53 @@
+"use client";
+import React from 'react';
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+
+
+
+const AddPostClient = () => {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await fetch("http://localhost:3001/api/posts", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ title, description }),
+            });
+            const data = await res.json();
+            console.log("Post created:", data);
+
+            setTitle("");
+            setDescription("");
+
+        } catch (error) {
+            console.error("Error creating post:", error);
+        }
+
+    }
+
+    return (
+        <div className="mx-auto max-w-6xl min-h-screen px-12 pt-24">
+            <h1 className="text-2xl font-semibold mb-6">Create New Post</h1>
+
+            <form onSubmit={handleSubmit} className="space-y-4">
+
+                <Input placeholder="Post title" onChange={(e) => setTitle(e.target.value)} value={title} />
+                <Textarea placeholder="Post content..." onChange={(e) => setDescription(e.target.value)} value={description} className="h-40" />
+                <Button type="submit">Create Post</Button>
+            </form>
+        </div>
+    );
+};
+
+export default AddPostClient;
--- src/app/(protected)/add-post/page.jsx
+++ src/app/(protected)/add-post/page.jsx
@@ -1,54 +1,13 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import AddPostClient from "./AddPostClient";
 
 
+export default async function EditPostPage({ params }) {
+  const session = await getServerSession(authOptions);
 
+  if (!session) redirect("/login");
 
-export default function AddPost() {
-
-
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState(""); 
-
-
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const res = await fetch("http://localhost:3001/api/posts", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ title, description }),
-      });
-      const data = await res.json();
-      console.log("Post created:", data);
-
-      setTitle("");
-    setDescription("");
-
-    } catch (error) {
-      console.error("Error creating post:", error);
-    }
-
-  }
-
-  return (
-    <div className="mx-auto max-w-6xl min-h-screen px-12 pt-24">
-      <h1 className="text-2xl font-semibold mb-6">Create New Post</h1>
-
-      <form onSubmit={handleSubmit} className="space-y-4">
-      
-        <Input placeholder="Post title" onChange = {(e) => setTitle(e.target.value)} value={title}/>
-        <Textarea placeholder="Post content..." onChange = {(e) => setDescription(e.target.value)} value={description} className="h-40"/>
-        <Button type="submit">Create Post</Button>
-      </form>
-    </div>
-  );
+  return <AddPostClient />;
 }
