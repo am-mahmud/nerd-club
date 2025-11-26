a0b5d0eacf3db1b0023b54cefb0e8794f2b64c2c
am-mahmud <am.mahmudasif@gmail.com>
2025-11-26 17:13:41 -0500

    Contact us page added

---
+++ src/app/(public)/contact/page.jsx
@@ -0,0 +1,12 @@
+import ContactSection from '@/components/contact';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <ContactSection></ContactSection>
+        </div>
+    );
+};
+
+export default page;
